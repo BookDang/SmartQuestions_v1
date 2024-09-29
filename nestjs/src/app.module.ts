0329e2d6eca14bc4e52fb58c35e7f7c01954e2af
Dang Sach <sachdang.dev@gmail.com>
2024-09-29 11:37:35 +0700

    Create the health check api

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { AppController } from '@/app.controller'
+import { AppService } from '@/app.service'
+import { HealthModule } from '@/health/health.module'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://root:smartquestionsmongodb@mongodb:27017/sq_db_v1?authSource=admin',
+      // 'mongodb://root:smartquestionsmongodb@mongodb:27017/sq_db_v1?authSource=admin',
+    ),
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
